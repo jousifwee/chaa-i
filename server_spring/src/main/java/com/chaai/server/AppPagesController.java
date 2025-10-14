c5b3bbc8003838d6c44ef1c8b61d08f5c78024e4
nwulff <nico.wulff@itz-rostock.de>
2025-10-14 13:59:39 +0200

    feat: add AppPagesController for handling application routing

---
+++ server_spring/src/main/java/com/chaai/server/AppPagesController.java
@@ -0,0 +1,24 @@
+package com.chaai.server;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppPagesController {
+
+    @GetMapping({"/app", "/app/"})
+    public String appRoot() {
+        return "forward:/app/index.html";
+    }
+
+    @GetMapping({"/app/secure", "/app/secure/"})
+    public String appSecure() {
+        return "forward:/app/secure/index.html";
+    }
+
+    @GetMapping({"/app/simple", "/app/simple/"})
+    public String appSimple() {
+        return "forward:/app/simple/index.html";
+    }
+}
+
